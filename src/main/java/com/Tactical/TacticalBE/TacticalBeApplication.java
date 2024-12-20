e84fe23e1294639e0de23928e7813a5e13b6b61f
aminajardali <167747357+aminajardali@users.noreply.github.com>
2024-12-20 17:45:21 +0200

    first commit

---
+++ src/main/java/com/Tactical/TacticalBE/TacticalBeApplication.java
@@ -0,0 +1,13 @@
+package com.Tactical.TacticalBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacticalBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacticalBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tactical/TacticalBE/TacticalBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tactical.TacticalBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacticalBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
